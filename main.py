0c028382cf1e600ea19bbabd3511b17d29fd048a
Lucas Rodrigues <109537651+LucasRodriguesEstorckPinto@users.noreply.github.com>
2025-06-01 18:55:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ async def enviar_sinal():
     )
 
     await bot.send_message(chat_id=CHAT_ID, text=mensagem_relatorio, parse_mode="Markdown")
-
+    await asyncio.sleep(1800)
     # 🔄 Resetar os contadores
     wins = 0
     losses = 0
